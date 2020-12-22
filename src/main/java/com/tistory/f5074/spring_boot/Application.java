04e5615300d8a1c59f8372fe5e0b4293b4a05c50
f5074 <oseongryu@gmail.com>
2020-12-22 09:40:05 +0900

    [Setting] logger

--- src/main/java/com/tistory/f5074/spring_boot/Application.java
+++ src/main/java/com/tistory/f5074/spring_boot/Application.java
@@ -5,19 +5,28 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @SpringBootApplication
 @MapperScan(value = {"com.tistory.f5074.spring_boot"})
+@EnableConfigurationProperties()
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+//        String[] beanNames = context.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for(String beanName : beanNames){
+//            System.out.println("[beanName]"+ beanName);
+//        }
     }
 
 //    @Bean
