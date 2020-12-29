3cd6c1b4af68bb2e4a5c40db0fabeee182b3875b
f5074 <oseongryu@gmail.com>
2020-12-29 11:22:09 +0900

    -

--- src/main/java/com/tistory/f5074/spring_boot/Application.java
+++ src/main/java/com/tistory/f5074/spring_boot/Application.java
@@ -3,28 +3,39 @@ package com.tistory.f5074.spring_boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
 
+import java.util.Arrays;
+
 @SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @MapperScan(value = {"com.tistory.f5074.spring_boot"})
 @EnableConfigurationProperties
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(Application.class, args);
-        ConfigurableEnvironment  env = (ConfigurableEnvironment) context.getEnvironment();
-        MutablePropertySources propertySources = env.getPropertySources();
-//        propertySources.addLast(new ResourcePropertySource("calsspath:app."));
-
-
-//        String[] beanNames = context.getBeanDefinitionNames();
-//        Arrays.sort(beanNames);
-//        for(String beanName : beanNames){
-//            System.out.println("[beanName]"+ beanName);
+//        ApplicationContext context = SpringApplication.run(Application.class, args);
+//        ConfigurableEnvironment  env = (ConfigurableEnvironment) context.getEnvironment();
+//        MutablePropertySources propertySources = env.getPropertySources();
+//        //        propertySources.addLast(new ResourcePropertySource("calsspath:app."));
+//
+//
+////        String[] beanNames = context.getBeanDefinitionNames();
+////        Arrays.sort(beanNames);
+////        for(String beanName : beanNames){
+////            System.out.println("[beanName]"+ beanName);
 //        }
+
+        ConfigurableApplicationContext ac = SpringApplication.run(Application.class, args);
+//        Arrays.stream(ac.getBeanDefinitionNames()).forEach(System.out::println);
+
+
+
     }
 
 //    @Bean
